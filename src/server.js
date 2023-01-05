2856fbf341fa2a76e6b1e74e97a59dff00bc7c97
2yunseong <dbsdltjd3701@naver.com>
2023-01-06 06:27:15 +0900

    feat: socket io 서버 단 기본 예제 실습
    - socket.on

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import WebSocket from 'ws';
+import SocketIO from 'socket.io';
 import express from 'express';
 
 const app = express();
@@ -16,49 +16,17 @@ app.get('/*', (rep, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`open http web server`);
 
-// 서버가 http, ws 프로토콜을 사용할 수 있게 만듬
-// 두개의 프로토콜이 동일한 포트를 공유
-// 이제 서버는 ws, http 두개의 프로토콜을 이해할 수 있음
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
 
-const handleConnectionClose = () => {
-  console.group('onConnectionClose');
-  console.log('disconnect from browser');
-  console.groupEnd();
-};
-
-/*
-  fake DB
-  현재 서버에 연결되어있는 소켓들
-*/
-const sockets = [];
-
-const unPackMessage = (message) => {
-  return JSON.parse(message);
-};
-
-// on : 어떤 이벤트가 발생했을 때 실행되는 것
-wss.on('connection', (socket) => {
-  console.log('Connect to Browser');
-  sockets.push(socket);
-  socket['nickname'] = 'guest';
-  socket.on('close', handleConnectionClose);
-  socket.on('message', (message) => {
-    const parsed = unPackMessage(message.toString('utf8'));
-    switch (parsed.type) {
-      case 'message':
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${parsed.payload}`)
-        );
-        break;
-      case 'nickname':
-        socket['nickname'] = parsed.payload;
-        break;
-      default:
-        break;
-    }
+// 연결 될 때
+io.on('connection', (socket) => {
+  socket.on('room', (msg, done) => {
+    console.log(msg);
+    // 프론트에서 emit에 전달해준 콜백함수가 실행됨
+    // 매개변수를 전달해 줄수도 있다! (매개변수는 문자열, 객체 등 모두 지원)
+    done('hello');
   });
 });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
