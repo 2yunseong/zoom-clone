a414aec35b943f323376c9bbdcd3ace69fa72638
2yunseong <dbsdltjd3701@naver.com>
2023-01-07 22:28:28 +0900

    feat: nickname 기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,26 +8,56 @@ room.hidden = true;
 
 let roomName;
 
-const showRoom = (roomName) => {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector('h3');
-  h3.innerText = `Room ${roomName}`;
-};
 const handleRoomSubmit = (e) => {
   e.preventDefault();
   const input = form.querySelector('input');
   // 채팅방 설정 emit(event, payload, callbackfn)
   // 서버에서 실행이 완료 되면, front 에서 callbackfn 이 실행됨.
-  socket.emit('enter_room', { payload: input.value }, showRoom);
+  socket.emit('enter_room', input.value, showRoom);
+};
+
+const handleMessageSubmit = (e) => {
+  e.preventDefault();
+  const input = room.querySelector('#msg').querySelector('input');
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${input.value}`);
+    input.value = '';
+  });
+};
+
+const handleNickNameSubmit = (e) => {
+  e.preventDefault();
+  const input = room.querySelector('#name').querySelector('input');
+  socket.emit('nickname', input.value);
+};
+
+const showRoom = (name) => {
+  welcome.hidden = true;
+  room.hidden = false;
+  roomName = name;
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector('#msg');
+  const nameForm = room.querySelector('#name');
+  msgForm.addEventListener('submit', handleMessageSubmit);
+  nameForm.addEventListener('submit', handleNickNameSubmit);
+};
+
+const addMessage = (msg) => {
+  const ul = room.querySelector('ul');
+  const li = document.createElement('li');
+  li.innerText = msg;
+  ul.appendChild(li);
 };
 
 form.addEventListener('submit', handleRoomSubmit);
 
-socket.on('welcome', () => {
-  console.log('emit welcome event');
-  const ul = room.querySelector('ul');
-  const li = document.createElement('li');
-  li.innerText = 'Someone joined!';
-  ul.appendChild(li);
+socket.on('welcome', (nickname) => {
+  addMessage(`${nickname}: Join.`);
 });
+
+socket.on('bye', (nickname) => {
+  addMessage(`${nickname}: left.`);
+});
+
+socket.on('new_message', addMessage);
--- src/server.js
+++ src/server.js
@@ -21,14 +21,28 @@ const io = SocketIO(httpServer);
 
 // 연결 될 때
 io.on('connection', (socket) => {
-  socket.on('enter_room', (room, done) => {
-    const roomName = room.payload;
+  socket['nickname'] = 'guest';
+  socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
     // 프론트에서 emit에 전달해준 콜백함수가 실행됨
     // 매개변수를 전달해 줄수도 있다! (매개변수는 문자열, 객체 등 모두 지원)
     done(roomName);
-    socket.to(roomName).emit('welcome');
+    socket.to(roomName).emit('welcome', socket.nickname);
   });
+
+  socket.on('disconnecting', () => {
+    const enterRooms = socket.rooms;
+    enterRooms.forEach((room) => {
+      socket.to(room).emit('bye', socket.nickname);
+    });
+  });
+
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}:${msg}`);
+    done();
+  });
+
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
 
 httpServer.listen(3000, handleListen);
